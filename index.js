bf7da4c4126889cfff928f07b0cbf832445d020f
n8vwfd9dy7-coder <n8vwfd9dy7@privaterelay.appleid.com>
2026-06-03 20:59:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
